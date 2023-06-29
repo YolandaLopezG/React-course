52cad59f3f1561b096a1d675f0b3d136ca8221b0
YolandaLopezG <ylg.10502@gmail.com>
2023-06-29 14:39:06 -0600

    Buttons comps

---
+++ comps/src/App.js
@@ -0,0 +1,45 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button primary rounded outline>
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Buy now!
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          Hide Adds!
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded>
+          Hello!
+        </Button>
+      </div>
+      <div>
+        <Button success outline rounded>
+          Look here!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline rounded>
+          Don't click here!
+        </Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          See deal!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
