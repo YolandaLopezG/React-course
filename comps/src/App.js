7c46e81510be12063391f848ec4fb973aec13bee
YolandaLopezG <ylg.10502@gmail.com>
2023-06-30 14:35:29 -0600

    icons

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,30 +1,42 @@
 import Button from "./Button";
+import {
+  BsBell,
+  BsBugFill,
+  BsCashCoin,
+  BsCloudLightningRain,
+  BsController,
+} from "react-icons/bs";
 
 function App() {
   return (
     <div>
       <div>
         <Button primary rounded outline>
+          <BsBugFill />
           Click me!
         </Button>
       </div>
       <div>
         <Button secondary rounded>
+          <BsCashCoin />
           Buy now!
         </Button>
       </div>
       <div>
         <Button warning outline>
+          <BsBell />
           Hide Adds!
         </Button>
       </div>
       <div>
         <Button danger rounded>
+          <BsCloudLightningRain />
           Hello!
         </Button>
       </div>
       <div>
         <Button success outline rounded>
+          <BsController />
           Look here!
         </Button>
       </div>
