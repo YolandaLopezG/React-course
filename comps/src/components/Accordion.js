24341714f91008ea110b863d1d663a41239b5e38
YolandaLopezG <ylg.10502@gmail.com>
2023-07-04 10:29:53 -0600

    expand and collapse accordion logic

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { BsChevronLeft, BsChevronDown } from "react-icons/bs";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    const icon = (
+      <span>{isExpanded ? <BsChevronDown /> : <BsChevronLeft />}</span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
