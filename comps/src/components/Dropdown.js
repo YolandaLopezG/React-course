d0fe3eeb8a34a5ccbcb0076f65be0fda562fb1ac
unknown <ylg.10502@gmail.com>
2023-07-10 13:29:19 -0600

    dropdown show

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,5 +1,20 @@
-function Dropdown({options}) {
-  return <h1>hola</h1>
+import { useState } from "react";
+
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  const renderdOptions = options.map((option) => {
+    return <div key={option.value}>{option.value}</div>;
+  });
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderdOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
