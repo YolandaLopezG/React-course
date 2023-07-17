682079c154b135f9176c4418b48709427635df4e
unknown <ylg.10502@gmail.com>
2023-07-17 10:33:51 -0600

    Diving and styling modal

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 function ModalPage() {
   const [showModal, setShowModal] = useState(false);
+
   const handlerClick = () => {
     setShowModal(true);
   };
@@ -11,12 +12,25 @@ function ModalPage() {
   const handleClose = () => {
     setShowModal(false);
   };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>I accept</Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here some text</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button onClick={handlerClick} primary>
         Open modal
       </Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
     </div>
   );
 }
