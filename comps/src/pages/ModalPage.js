e4207f42a1d576365fe0cee23e3d204c7ced07ee
unknown <ylg.10502@gmail.com>
2023-07-17 10:14:16 -0600

    Show modal

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -1,9 +1,18 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 
 function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+  const handlerClick = () => {
+    setShowModal(true);
+  };
   return (
     <div>
-      <Modal />
+      <Button onClick={handlerClick} primary>
+        Open modal
+      </Button>
+      {showModal && <Modal />}
     </div>
   );
 }
